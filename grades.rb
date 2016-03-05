682a6a465419cdc28bbf41c65f74be82d195b135
agnik <reira@wp.pl>
2016-03-05 21:41:30 +0100

    grades1 added

---
+++ grades.rb
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5, 6]
+
+for num in numbers
+  puts(num)
+end
+
+# for number in numbers
+  # puts(number + 10)
+# end
+
+grades = {1 => "very bad", 2 => "bad", 3 => "mediocre", 4 => "good", 5 => "very good", 6 => "excellent"}
+
+for (grade, grade_description) in grades
+  puts("grade " + grade.to_s + " means " + grade_description)
+end
